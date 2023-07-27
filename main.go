786959cfba052085d0ff7e78f6cffcfd65bfb171
Klaus Breyer <kb@v01.io>
2023-07-27 09:29:28 +0200

    New Render Method, No Yield

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package grr
+package main
 
 import (
 	"bytes"
@@ -36,12 +36,8 @@ func Extend(s interface{}) map[string]interface{} {
 
 // Render returns a template.HTML from a template string and data.
 // data interface{} so that it can use a map or a struct.
-func Render(tmpl string, data interface{}, yield ...template.HTML) template.HTML {
-	t := template.Must(template.New("").Funcs(template.FuncMap{
-		"yield": func() template.HTML {
-			return joinHTML(yield, "")
-		},
-	}).Parse(tmpl))
+func Render(data interface{}, tmpl string) template.HTML {
+	t := template.Must(template.New("").Funcs(template.FuncMap{}).Parse(tmpl))
 
 	var buf bytes.Buffer
 	err := t.Execute(&buf, data)
@@ -64,30 +60,41 @@ func Flatten(items []template.HTML) template.HTML {
 	return all
 }
 
-func getNav(data DataNav, yield ...template.HTML) template.HTML {
-	dataMap := Extend(data)
-	dataMap["Foot"] = getFoot(DataFoot{Copy: "© 2021"})
-	return Render(`
-	<nav class="shadow sticky top-0 z-10">
+// my favorite. Because it is the most type save and the most readable and it does no hack and
+// i dont like typing "" all the time.
+func getNav(data DataNav) template.HTML {
+	return Render(struct {
+		Foot          template.HTML
+		InputVariable string
+		OtherInput    string
+	}{
+		getFoot(DataFoot{Copy: "© 2021"}),
+		data.InputVariable,
+		data.OtherInput,
+	},
+		`
+    <nav class="shadow sticky top-0 z-10">
+        {{.InputVariable}}
+        {{.OtherInput}}
 		{{.Foot}}
-		{{.InputVariable}}
-		{{.OtherInput}}
-		{{yield}}
-	</nav>
-	`, dataMap, yield...)
+    </nav>
+    `)
 }
 
 type DataFoot struct {
 	Copy string
 }
 
-func getFoot(data DataFoot, yield ...template.HTML) template.HTML {
-	return Render(`
+func getFoot(data DataFoot) template.HTML {
+	return Render(struct {
+		Copy string
+	}{
+		data.Copy,
+	}, `
 	<footer>
 		{{.Copy}}
-		{{yield}}
 	</footer>
-	`, Extend(data), yield...)
+	`)
 }
 
 func main() {
@@ -95,6 +102,6 @@ func main() {
 		InputVariable: "Eingabe 1",
 		OtherInput:    "Eingabe 2",
 	}
-	html := getNav(navData, "Yield 1", "Yield 2")
+	html := getNav(navData)
 	log.Println(html)
 }
